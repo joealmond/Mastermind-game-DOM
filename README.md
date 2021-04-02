5192826693e0798b9e0d8649fdfb5eafc5ccaba2
joealmond <jozsef.mandula@gmail.com>
2021-04-02 13:12:18 +0200

    Console verzió kész.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Mastermind
+
+Todo:
+
+Tervek:
+
+- kód egyszerűsítés
+- html text felület
+- html színes felület
+- html tologatós felület
+
+Feature upgrade:
+
+- választható több számjegyű játék
--- src/main.js
+++ src/main.js
@@ -6,22 +6,13 @@ const secretCode = generateSecret();
 
 // Function declarations:
 
-/**
- * Legenerál egy '0' és '9' közötti számjegyet
- *
- * @return string
- */
-
 function generateDigit() {
+  // Legenerál egy '0' és '9' közötti számjegyet
   return String(Math.trunc(Math.random() * 10));
 }
 
-/**
- * Legenerálja a négyjegyű titkos megoldást.
- *
- * @return Array of '0'...'9' - A négy elemű titkos megoldás
- */
 function generateSecret() {
+  // Legenerálja a négyjegyű titkos megoldást.
   let result = [];
   let index;
   for (index = 0; index < 4; ++index) {
@@ -31,6 +22,7 @@ function generateSecret() {
 }
 
 function readGuess() {
+  // Bekéri a tippet a felhasználótól ami egy negyjegyű egész szám
   let guess, isNumber;
   do {
     guess = prompt(
@@ -48,6 +40,7 @@ function readGuess() {
 }
 
 function getRedCount(guess) {
+  // Összeveti mely számjegyek vannak a helyén és visszaadja a találatok összegét.
   let redCount = 0;
   let i;
   for (i = 0; i < 4; i++) {
@@ -59,32 +52,45 @@ function getRedCount(guess) {
 }
 
 function getWhiteCount(guess) {
+  // Összevet hogy vannak-e olyan számok amelyek nincsenek a helyükön de előfordulnak.
+  // Minden számjegy csak egyszer szerepelhet, miközben a helyükön lévő számjegyek nem szerepelhetnek.
   let whiteCount = 0;
-  let whiteCountHelper = new Set();
-  let secretCodeHelper = secretCode.slice(); // le kell másolni, hiszen referencia hivatkozás!!!
-  let guessHelper = guess.slice();
+  let whiteCountHelper = new Set(); // egy halmazban tárolom átmenetileg az eredmény számokat.
+  let secretCodeHelper = secretCode.slice(); // a stringet tömb objektummá alakítom és mivel így már referencia érték ezét készítek egy másolatot, hogy az eredei érték ne módosuljon.
+  let guessHelper = guess.slice(); // a stringet tömb objektummá alakítom és mivel így már referencia érték ezét készítek egy másolatot, hogy az eredeti érték ne módosuljon.
   let i, j;
   for (i = 0; i < secretCodeHelper.length; ++i) {
-    if (secretCodeHelper[i] === guessHelper[j]) {
-      whiteCountHelper.add(guess[i]);
+    // az egyásba ágyazott ciklusok gondoskodnak aról hogy a két tömb minden eleme össz elegyen vetve egymással.
+    if (i === j && secretCodeHelper[i] == guessHelper[j]) {
+      // ha találok helyi értéken egyezést akkor azt kiveszem a tömbökből.
+      secretCodeHelper.splice(i, 1);
+      guessHelper.splice(j, 1);
+      if (i >= 0) {
+        // mivel kivettem egy elemt a tömbökből az rövidebb lett, ezt kompenzálni kell vigyázva arra hogy az érték ne legyen negatív szám.
+        i = i - 1;
+      }
+    } else if (secretCodeHelper[i] === guessHelper[j]) {
+      whiteCountHelper.add(guess[j]);
     }
     for (j = 0; j < guessHelper.length; ++j) {
-      if (secretCodeHelper[i] === guessHelper[j]) {
+      if (i === j && secretCodeHelper[i] == guessHelper[j]) {
+        secretCodeHelper.splice(i, 1);
+        guessHelper.splice(j, 1);
+        if (j >= 0) {
+          j = j - 1;
+        }
+      } else if (secretCodeHelper[i] === guessHelper[j]) {
         whiteCountHelper.add(guess[j]);
       }
     }
-    if (j >= 0) {
-      j = j - 1;
-    }
   }
-  if (i >= 0) {
-    i = i - 1;
-  }
-  whiteCount = whiteCountHelper.size;
+
+  whiteCount = whiteCountHelper.size; // megnézem az eredményhalmaz méretét majd visszadom azt
   return whiteCount;
 }
 
 function isGameWon(redCount) {
+  // ha a helyiértéken való találatok száma eléri a 4-et akkor azt visszadaom.
   if (redCount !== 4) {
     return;
   } else return redCount === 4;
@@ -95,9 +101,8 @@ function isGameWon(redCount) {
 console.log(
   "Titkos kód: " + secretCode + "    ...Tesztelési célokra megjelenítve..."
 );
-// document.body.innerHTML = `<p>Titkos kód: ${secretCode}<p>`;
 function gameloop() {
-  while (actualTurns != 0) {
+  while (true) {
     let guess = readGuess();
     if (guess === "exit") {
       console.log("Viszlát!");
@@ -114,9 +119,10 @@ function gameloop() {
     }
     actualTurns = actualTurns - 1;
     console.log("Hátralévő próbálkozások: " + TURNS + "/" + actualTurns);
-  }
-  if (actualTurns === 0) {
-    console.log("Nem nyertél");
+    if (actualTurns === 0) {
+      console.log("Nem nyertél");
+      break;
+    }
   }
 }
 
